aa6a09284ab3366d113f87f4e68d5cfd3d179257
Kirill Drozdov <kirilldrozd12@gmail.com>
2025-11-09 23:16:35 +0500

    Initial Commit

---
+++ Widget/WidgetApp.swift
@@ -0,0 +1,32 @@
+//
+//  WidgetApp.swift
+//  Widget
+//
+//  Created by Kirill Drozdov on 09.11.2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct WidgetApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
